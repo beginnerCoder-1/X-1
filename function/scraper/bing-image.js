a2c9375152f3190fc7f449594596b9218c9b65c5
beginnerCoder-1 <nokoswahyu@gmail.com>
2024-12-22 07:22:39 +0800

    Add files via upload

---
+++ function/scraper/bing-image.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+async function bingImage(prompt, model) {
+  const url = `https://ryzendesu.vip/api/ai/v2/text2img?prompt=${prompt}&model=`;
+  try {
+    const response = await axios.get(url, {
+      responseType: "arraybuffer", 
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = bingImage;
