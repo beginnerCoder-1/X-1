2dd5078c13e8b92e2624ecba5133b7e7aef0ef69
beginnerCoder-1 <nokoswahyu@gmail.com>
2024-12-21 19:24:32 +0800

    Delete function/index.js

--- function/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	blackboxAIChat: require('./scraper/blackboxAIChat'),
-	smartContract: require('./scraper/smartContract'),
-	ragBot: require('./scraper/ragBot'),
-	degreeGuru: require('./scraper/degreeGuru'),
-	tiktok: require('./scrape/tiktok'),
-	brat: require('./scrape/brat')
-}
